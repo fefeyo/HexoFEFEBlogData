b0f582349acb4fe1a4b6881d2ef610efaf8f94d8
sakatayu <sakatayu@team-lab.com>
2018-05-25 10:19:24 +0900

    revival_themes

---
+++ themes/mytheme/source/js/layout/header.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function() {
+    (function() {
+        var headerEle = document.getElementById('page-header');
+
+        function toggleNavStyle() {
+            var scrollTop = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+
+            headerEle.classList[ scrollTop > 30 ? 'add' : 'remove' ]('page__header--small');
+        }
+
+        headerEle && document.addEventListener('scroll', toggleNavStyle);
+    })();
+
+    (function() {
+        var
+            btn = document.querySelector('button.page__menu-btn'),
+            menu = document.querySelector('nav.page__nav');
+
+        if (btn && menu) {
+            var packMenu = new Pack(menu);
+
+            packMenu.base('js-open').transfrom('page__nav--open');
+
+            btn.addEventListener('click', function() {
+                packMenu.toggle();
+            });
+        }
+    })();
+    // TODO: タイトルとかにアニメーションつける？
+});
